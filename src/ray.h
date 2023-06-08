cbc2a6d462cb9f863edc11225b31465e784d1884
chuckrak <chuckrak1@gmail.com>
2023-06-07 21:56:02 -0700

    nothing works.

--- src/ray.h
+++ src/ray.h
@@ -6,7 +6,9 @@
 class ray {
     public:
         ray() {}
-        ray(const point3& origin, vec3& direction) : orig(origin), dir(direction) {}
+        ray(const point3& origin, vec3& direction) : 
+            orig(origin), dir(direction) 
+        {}
 
         point3 origin() const {return orig; }
         vec3 direction() const {return dir; }
@@ -16,9 +18,6 @@ class ray {
         }
 
 
-
-
-    public:
         point3 orig;
         vec3 dir;
 
