69babb8996e5c466aa3d06c621729a3f51cea78e
chuckrak <chuckrak1@gmail.com>
2023-06-10 18:33:16 -0700

    wipwipwip

--- src/main.cc
+++ src/main.cc
@@ -1,29 +1,19 @@
+#include "rt.h"
+
+
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
-#include <memory>
 
-
-double hit_sphere(const point3& center, double radius, const ray& r){
-    vec3 oc = r.origin() - center;
-    
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant < 0) ? -1.0 : (-b - sqrt(discriminant)) / (2.0 * a);
-}
-
-color ray_color(const ray& r){
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0){
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world){
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)){
+        return 0.5 * (rec.normal + color(1,1,1));
     }
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 
 }
@@ -40,6 +30,11 @@ int main () {
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
     //Camera
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
@@ -50,8 +45,7 @@ int main () {
     auto vertical = vec3(0, viewport_height, 0);
     auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
     
-
-
+    //Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for(int j = image_height - 1; j >= 0; j--){
         std::cerr << "\rScanlines remaining" << j << ' ' << std::flush;
@@ -62,7 +56,7 @@ int main () {
             vec3 dir = lower_left_corner + u*horizontal + v*vertical - origin;
             ray r = ray(orig, dir);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
