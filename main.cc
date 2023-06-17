4863c66e21e309839efdcc31447b88ce6705e7ba
Chuck Rak <crak@abnormalsecurity.com>
2023-06-17 11:49:08 -0700

    sphere

--- main.cc
+++ main.cc
@@ -5,16 +5,53 @@
 #include <vector>
 #include "geometry.h"
 
+struct Sphere {
+    Vec3f center;
+    float radius;
 
-void render() {
+    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+
+    bool ray_intersect(const Vec3f orig, const Vec3f &dir, float &t0) const {
+        Vec3f L = center - orig;
+        float tca = L * dir;
+        float d2 = L*L - tca*tca;
+        if (d2 > radius * radius) return false;
+        float thc = sqrtf(radius*radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+        if(t0 < 0){
+            t0 = t1;
+        }
+        return t0 >= 0;
+    }
+};
+
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere){
+    const Vec3f sphere_color = Vec3f(0.4, 0.4, 0.3);
+    const Vec3f background_color = Vec3f(0.2, 0.7, 0.8);
+    float sphere_dist = std::numeric_limits<float>::max();
+    if (!sphere.ray_intersect(orig, dir, sphere_dist)){
+        return background_color;
+    }
+    return sphere_color;
+}
+
+
+
+void render(const Sphere &sphere) {
     const int width = 1024;
     const int height = 768;
     const int total_pixels = height * width;
+    const int fov = M_PI/2.0;
     std::vector<Vec3f> framebuffer(width*height);
 
+    #pragma omp parallel for
     for (size_t j = 0; j < height; j++){
         for (size_t i = 0; i < width; i++){
-            framebuffer[i+j*width] = Vec3f(j/float(height), i/float(width), 0);
+            float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov/2.0) * width/(float)height;
+            float y = -(2*(j+0.5) / (float)height - 1) * tan(fov/2.0);
+            Vec3f dir = Vec3f(x, y, -1).normalize();
+            framebuffer[i+j*width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
         }
     }
 
@@ -30,6 +67,7 @@ void render() {
 }
 
 int main() {
-    render();
+    Sphere sphere(Vec3f(-3, 0, -16), 2);
+    render(sphere);
     return 0;
 }
