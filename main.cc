275313df739037f73ffcb56281e9a0a745fab343
Chuck Rak <crak@abnormalsecurity.com>
2023-06-17 13:18:01 -0700

    fix

--- main.cc
+++ main.cc
@@ -7,7 +7,7 @@
 
 struct Material {
     Material(const Vec2f &a, const Vec3f &color, const float &spec) : albedo(a), diffuse_color(color), specular_exponent(spec) {}
-    Material() : diffuse_color() {}
+    Material() : albedo(1, 0), diffuse_color(), specular_exponent() {}
     Vec2f albedo;
     Vec3f diffuse_color;
     float specular_exponent;
